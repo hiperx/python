7adf6bbac89f7701eab2c76b9ada85e01beda5f1
hiperx <sbrokerclub@gmail.com>
2019-12-18 17:01:40 +0100

    Oblicznie sredniejH

---
+++ srednia.py
@@ -0,0 +1,28 @@
+import csv
+from collections import defaultdict
+
+
+przedmioty = defaultdict(list)
+
+
+with open('oenkil.csv') as plik:
+
+    dane = csv.reader(plik, delimiter=',')
+    header = next(dane)
+
+    for linia in dane:
+        przedmioty[linia[0]].append([float(linia[1]), int(linia[2])])
+
+
+for przedmiot in przedmioty:
+
+    iloczyn = 0
+    iloraz = 0
+
+    for ocena in przedmioty[przedmiot]:
+        iloczyn += ocena[0]*ocena[1]
+        iloraz += ocena[1]
+
+    srednia = iloczyn/iloraz
+
+    print(przedmiot+' = '+str(round(srednia, 1)) + " ocena po zaokrgleniu : " + str(round(srednia)))
